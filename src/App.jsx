445913123fb0d9040c2d9d27f0c9e8921bf381e0
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-24 23:52:39 +0100

    Initial app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import {Switch, Route, Link} from 'react-router-dom'
+import { Layout, Space, Typography } from 'antd'
+
 
 function App() {
 
   return (
 
-    <div></div>
+    <div className='app'>
+
+      <div className='navbar'>
+
+      </div>
+
+      <div className='main'>
+
+      </div>
+
+      <div className='footer'>
+
+      </div>
+
+    </div>
 
   )
 }
