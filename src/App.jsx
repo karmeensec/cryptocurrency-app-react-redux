03d00f9831a145a3f1e845036bfb4391e73c70a0
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-25 19:21:26 +0100

    Routing through main components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { Route, Link, Routes} from 'react-router-dom'
 import { Layout, Space, Typography } from 'antd'
-import { Navbar } from './components';
+import { Navbar, HomePage, Exchanges, Cryptocurrencies, Cryptodetails, News } from './components';
 
 function App() {
 
@@ -15,6 +15,24 @@ function App() {
       </div>
 
       <div className='main'>
+
+        <Layout>
+          <div className='routes'>
+            <Routes>
+
+              <Route exact path='/' element={ <HomePage />} ></Route>
+
+              <Route exact path='/exchanges' element={ <Exchanges /> } ></Route>
+
+              <Route exact path='/cryptocurrencies' element={ <Cryptocurrencies /> } ></Route>
+
+              <Route exact path='/crypto/:coinId' element={ <Cryptodetails /> } ></Route>
+
+              <Route exact path='/news' element={ <News /> } ></Route>
+
+            </Routes>
+          </div>
+        </Layout>
 
       </div>
 
