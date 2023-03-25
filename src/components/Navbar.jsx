827a5c49305b7d25becb19a9784a710e55a56fa4
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-25 19:21:14 +0100

    Menus

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,6 +21,26 @@ const Navbar = () => {
             </Button> */}
         </div>
 
+        <Menu theme='dark' >
+          
+            <Menu.Item icon={ <HomeOutlined /> } >
+              <Link to='/' >Home</Link>
+            </Menu.Item>
+
+            <Menu.Item icon={ <FundViewOutlined /> } >
+              <Link to='/cryptocurrencies' >Cryptocurrencies</Link>
+            </Menu.Item>
+
+            <Menu.Item icon={ <MoneyCollectOutlined /> } >
+              <Link to='/exchanges' >Exchanges</Link>
+            </Menu.Item>
+
+            <Menu.Item icon={ <BulbOutlined /> } >
+              <Link to='/news' >News</Link>
+            </Menu.Item>
+
+        </Menu>
+
     </div>
 
   )
