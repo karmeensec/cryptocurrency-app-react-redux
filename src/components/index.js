ce55c6b0e738ac9fe0ce356e218229505344a20e
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-25 19:21:04 +0100

    Exporting main components

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
 export { default as Navbar } from "./Navbar";
-
+export { default as HomePage } from "./HomePage";
+export { default as Exchanges } from "./Exchanges";
+export { default as Cryptocurrencies } from "./Cryptocurrencies";
+export { default as Cryptodetails } from "./Cryptodetails";
+export { default as News } from "./News";
