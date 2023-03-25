054865aa6e459a9c0d97b6206ea28f36900f7c47
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-25 12:55:50 +0100

    Initial navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,28 @@
+import { MoneyCollectOutlined } from '@ant-design/icons'
+import { BulbOutlined, FundViewOutlined, HomeOutlined, MenuFoldOutlined } from '@ant-design/icons/lib/icons'
+import { Avatar, Button, Menu, Typography } from 'antd'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import icon from '../assets/cryptocurrency.png';
 
 const Navbar = () => {
 
   return (
 
-    <div>Navbar</div>
-    
+    <div className='nav-container'>
+        
+        <div className='logo-container'>
+            <Avatar src={icon} size='large' />
+            <Typography.Title level={2} className='logo'>
+                <Link to='/' >CryptoMoon</Link>
+            </Typography.Title>
+            {/* <Button className='menu-control-container'>
+
+            </Button> */}
+        </div>
+
+    </div>
+
   )
 }
 
