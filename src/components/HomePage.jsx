96e4707bada00c05e7346c1e1e330f4ef3394c41
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-25 20:27:11 +0100

    Homepage statistics

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,27 @@
+import { Col, Row, Statistic, Typography } from 'antd'
+import millify from 'millify'
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+const { Title } = Typography;
 
 const HomePage = () => {
 
   return (
 
-    <div>HomePage</div>
+    <>
+
+      <Title level={2} className='heading'  >Global Crypto Statistics</Title>
+
+      <Row>
+        <Col span={12} > <Statistic title='Total Cryptocurrencies' value='5' /> </Col>
+        <Col span={12} > <Statistic title='Total Exchanges' value='5' /> </Col>
+        <Col span={12} > <Statistic title='Total Market Cap' value='5' /> </Col>
+        <Col span={12} > <Statistic title='Total 24h Volume' value='5' /> </Col>
+        <Col span={12} > <Statistic title='Total Markets' value='5' /> </Col>
+      </Row>
+    
+    </>
     
   )
 }
