df7448e395547203cdd2734a364d2e7c68bd4a9c
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-27 00:33:19 +0200

    connecting cryptonews with the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { cryptoApi } from "../services/cryptoApi";
+import { cryptoNewsApi } from "../services/cryptoNewsApi";
+
+
+const middlewares = [cryptoApi.middleware, cryptoNewsApi.middleware];
 
 export default configureStore ({
 
     reducer: {
         [cryptoApi.reducerPath]: cryptoApi.reducer,
+        [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
     },
 
-    middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(cryptoApi.middleware),
+    // middleware: (getDefaultMiddleware) =>
+    // getDefaultMiddleware().concat(cryptoApi.middleware),
 
+    // middleware: (getDefaultMiddleware) =>
+    // getDefaultMiddleware().concat(cryptoNewsApi.middleware),
+
+    middleware: getDefaultMiddleware().concat(middlewares),
 })
