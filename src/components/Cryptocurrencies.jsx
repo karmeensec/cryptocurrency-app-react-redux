591a31179d42e1e41584ffa01cf9b85045080eb2
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-26 13:30:02 +0200

    Initial state for cryptos

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import { Card, Col, Row, Input } from 'antd'
+import millify from 'millify'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const Cryptocurrencies = () => {
 
+  const {data: cryptoList, isFetching} = useGetCryptosQuery();
+  const [cryptos, setCryptos] = useState(cryptoList?.data?.coins);
+  console.log(cryptos);
+
   return (
 
-    <div>Cryptocurrencies</div>
+    <>
+    
+      <Row gutters={[32, 32]} className='crypto-card-container' >
+        
+      </Row>
+
+    </>
     
   )
 }
