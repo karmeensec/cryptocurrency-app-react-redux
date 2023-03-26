324d1bae4d4bc0b9d81c1b0b91c9659c74940d55
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-26 13:56:54 +0200

    mapping, rendering the currencies

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -14,8 +14,18 @@ const Cryptocurrencies = () => {
 
     <>
     
-      <Row gutters={[32, 32]} className='crypto-card-container' >
-        
+      <Row gutter={[32, 32]} className='crypto-card-container' >
+        {cryptos.map((currency)=> (
+            <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id} >
+              <Link to={`/crypto/${currency.id}`} >
+                <Card title={`${currency.rank} ${currency.name} `} extra={ <img className='crypto-image' src={currency.iconUrl} /> } hoverable >
+                  <p>Price: {millify(currency.price)} </p>
+                  <p>Daily Change: {millify(currency.change)}% </p>
+                  <p>Market Cap: {millify(currency.marketCap)} </p>
+                </Card>
+              </Link>
+            </Col>
+        ))}
       </Row>
 
     </>
