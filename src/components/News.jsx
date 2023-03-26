843cf4c5888103461a8041219336b67547db680e
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-27 00:15:02 +0200

    getting initial data from api, props

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,14 @@
+import { Avatar, Card, Col, Row, Select, Typography } from 'antd'
+import moment from 'moment/moment'
 import React from 'react'
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi'
 
-const News = () => {
+const { Title, Text } = Typography;
+const { Option } = Select;
+
+const News = ({ simpleVersion }) => {
+
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', counter: simpleVersion ? 10: 100 });
 
   return (
     
