678f2654864d5fb0b59c84b91ddbe71601652911
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-26 13:19:00 +0200

    configuring store

---
+++ src/app/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { cryptoApi } from "../services/cryptoApi";
+
+export default configureStore ({
+
+    reducer: {
+        [cryptoApi.reducerPath]: cryptoApi.reducer,
+    },
+
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(cryptoApi.middleware),
+
+})
