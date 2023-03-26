03ca340fe9df99973b8bc818572710fd7ce89445
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-26 13:19:30 +0200

    creating api

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,32 @@
+
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+
+    'X-RapidAPI-Key': '3722a6f74cmsh8ecb6399edd3c2bp1765f0jsnb50a922caba7',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+
+export const cryptoApi = createApi({
+
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+
+        getCryptos: builder.query({
+            query: ()=> createRequest('/coins')
+        })
+
+    })
+
+});
+  
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
