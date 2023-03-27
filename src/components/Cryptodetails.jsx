9b2e495e1a478749997c467cc75c0da9ae7c0cdc
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-27 20:11:34 +0200

    Initial state for time period, useParams

--- src/components/Cryptodetails.jsx
+++ src/components/Cryptodetails.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { Col, Row, Select, Typography } from 'antd'
+import HTMLReactParser from 'html-react-parser'
+import millify from 'millify'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { MoneyCollectOutlined, DollarCircleOutlined, FundViewOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckCircleOutlined, FieldNumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+
+const { Text, Title } = Typography;
+const { Option} = Select;
 
 const Cryptodetails = () => {
 
+  const {coinId} = useParams();
+  const [timePeriod, setTimePeriod] = useState('7 days');
+
   return (
 
-    <div>Cryptodetails</div>
+    <div>Cryptodetails {coinId} </div>
     
   )
 }
