6b0905ed4413e81eff9588b675bc3fc24b3094c2
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-27 13:05:45 +0200

    mapping through the news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -8,12 +8,26 @@ const { Option } = Select;
 
 const News = ({ simpleVersion }) => {
 
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', counter: simpleVersion ? 10: 100 });
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', counter: simpleVersion ? 6: 12 });
   console.log(cryptoNews);
+
+  if (!cryptoNews?.value) return 'Loading...';
 
   return (
     
-    <div>News</div>
+    <Row gutter={[24, 24]}>
+        {cryptoNews.value.map((news, i)=> (
+          <Col xs={24} sm={12} lg={8} key={i}>
+              <Card className='news-card' hoverable>
+                <a href={news.url} target='_blank' rel='noreferrer'>
+                  <div className='news-image-container'>
+                    <Title className='news-title' level={4}> {news.name} </Title>
+                  </div>
+                </a>
+              </Card>
+          </Col>
+        ))}
+    </Row>
     
   )
 }
