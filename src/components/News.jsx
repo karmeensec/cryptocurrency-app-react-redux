265969a345d82e4e9999429060c919abf968660c
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-27 18:16:21 +0200

    rendering all news on UI

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -5,6 +5,7 @@ import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi'
 
 const { Title, Text } = Typography;
 const { Option } = Select;
+const demo = '';
 
 const News = ({ simpleVersion }) => {
 
@@ -16,13 +17,30 @@ const News = ({ simpleVersion }) => {
   return (
     
     <Row gutter={[24, 24]}>
+
+      
+
         {cryptoNews.value.map((news, i)=> (
           <Col xs={24} sm={12} lg={8} key={i}>
               <Card className='news-card' hoverable>
                 <a href={news.url} target='_blank' rel='noreferrer'>
+
                   <div className='news-image-container'>
+
                     <Title className='news-title' level={4}> {news.name} </Title>
+                    <img style={{ maxWidth: '100px', maxHeight: '100px' }} src={news?.image?.thumbnail?.contentUrl || demo } alt="news" />
                   </div>
+                  <p>
+                    {news.description > 100 ? `${news.description.substring(0, 100)}...` : news.description}
+                  </p>
+                  <div className='provider-container'>
+                    <div>
+                      <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demo } alt='news' />
+                      <Text className='provider-name'> {news.provider[0]?.name} </Text>
+                    </div>
+                    <Text> {moment(news.datePublished).startOf('ss').fromNow()} </Text>
+                  </div>
+
                 </a>
               </Card>
           </Col>
