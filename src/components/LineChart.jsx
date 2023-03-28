f8d1fd5f17bf14272140690654a677a7eef7e454
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-28 15:29:02 +0200

    Initial LineChart

---
+++ src/components/LineChart.jsx
@@ -0,0 +1,16 @@
+import { Col, Row, Typography } from 'antd'
+import React from 'react'
+import { Line } from 'react-chartjs-2'
+
+const {Title} = Typography;
+
+const LineChart = () => {
+
+  return (
+    
+    <div>LineChart</div>
+
+  )
+}
+
+export default LineChart
