48960cddc4c89e4d37709a4a3bdac0661453e776
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-28 15:52:40 +0200

    rendering chart details on UI

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -4,11 +4,24 @@ import { Line } from 'react-chartjs-2'
 
 const {Title} = Typography;
 
-const LineChart = () => {
+const LineChart = ({ coinHistory, coinName, currentPrice }) => {
 
   return (
     
-    <div>LineChart</div>
+    <>
+    
+        <Row className='chart-header'>
+
+            <Title level={2} className='chart-title' > {coinName} Prices Chart </Title>
+
+            <Col className='price-container'>
+                <Title level={5} className='price-change' > {coinHistory?.data?.change} % </Title>
+                <Title level={5} className='current-price' > Current {coinName} Price: $ {currentPrice} </Title>
+            </Col>
+
+        </Row>
+
+    </>
 
   )
 }
