43b81f2303150f9a2a357111d010ce69a123173a
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-28 15:28:37 +0200

    Exporting default LineChart component

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Exchanges } from "./Exchanges";
 export { default as Cryptocurrencies } from "./Cryptocurrencies";
 export { default as Cryptodetails } from "./Cryptodetails";
 export { default as News } from "./News";
+export { default as LineChart } from "./LineChart";
