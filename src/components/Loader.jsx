09ca0029507b6e1a79f4bf19b8e8236d11c060c6
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-03-28 22:09:10 +0200

    Loader rendering

---
+++ src/components/Loader.jsx
@@ -0,0 +1,16 @@
+import { Spin } from 'antd'
+import React from 'react'
+
+
+const Loader = () => {
+
+  return (
+
+    <div className='loader'>
+        <Spin />
+    </div>
+
+  )
+}
+
+export default Loader
